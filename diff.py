de91ee26d36b19d0eeb09a0e426acec191afdcea
Zhi Wang <zhiwang@uchicago.edu>
2018-09-06 16:53:21 -0500

    combined two diff list to a dict, matchlist includes both and is a dict, added filter and exclude

--- diff.py
+++ diff.py
@@ -1,6 +1,5 @@
 from pdblib import parse_pdb_file, parse_pdb_line
 from .pdblist import PDBList
-#from sys import stderr
 
 def equal(lhs, rhs, tol=1e-6):
     """
@@ -27,7 +26,8 @@ def entry_compare(lhs, rhs, cmp_keys = ["x", "y", "z"]):
     """
     return not entry_match(lhs, rhs, cmp_keys)
 
-def diff_pdb(query, subject, match=entry_match, compare=entry_compare):
+def diff_pdb(query, subject, match=entry_match, compare=entry_compare,
+             filter=None, exclude=None):
     """
     calculate the difference between pdb files
 
@@ -38,6 +38,9 @@ def diff_pdb(query, subject, match=entry_match, compare=entry_compare):
     compare : in, a callable to determine whether two matching entries are 
               inequivalent, return True if not identical
 
+    filter  : a function that changes a query item, return an entry, comes first
+    exclude : a function that removes a query item, return True if excluding
+
     matchlist, the entries in subject file that match the query file
     diff1list, diff2lis, the entries where two files are different
     notfoundlist, the entries in the query file not found in the subject file
@@ -46,29 +49,34 @@ def diff_pdb(query, subject, match=entry_match, compare=entry_compare):
     pdb1 = parse_pdb_file(query)
     pdb2 = parse_pdb_file(subject)
 
-    #lpdb2 = len(pdb2)
-    matchlist = PDBList()
-    diff1list = PDBList()
-    diff2list = PDBList()
+    if filter is not None:
+        pdb1 = PDBList([filter(entry) for entry in pdb1])
+
+    matchlist = {
+            "query"   : PDBList(),
+            "subject" : PDBList(),
+                }
+    difflist = {
+            "query"   : PDBList(),
+            "subject" : PDBList(),
+               }
     notfoundlist = PDBList()
-    #idx2 = 0
+
     for entry in pdb1:
+        if exclude is not None and exclude(entry):
+            continue
         found = False
-        #for idx2 in range(idx2, lpdb2):
         for idx2 in range(len(pdb2)):
             if match(entry, pdb2[idx2]):
-                matchlist.append(pdb2[idx2])
+                matchlist["query"].append(entry)
+                matchlist["subject"].append(pdb2[idx2])
                 if compare(entry, pdb2[idx2]):
-                    diff1list.append(entry)
-                    diff2list.append(pdb2[idx2])
+                    difflist["query"].append(entry)
+                    difflist["subject"].append(pdb2[idx2])
                 del pdb2[idx2]
                 found = True
                 break
-        #if idx2 == lpdb2:
         if not found:
-            #print("Warning, entry not found:", entry, file=sys.stderr)
-            #matchlist.append(parse_pdb_line(""))
             notfoundlist.append(entry)
-            #return False, [], []
 
-    return matchlist, diff1list, diff2list, notfoundlist
+    return matchlist, difflist, notfoundlist
