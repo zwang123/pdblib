160ebd841600ef3f17b75e7b95b8d0c10a692c50
Zhi Wang <zhiwang@uchicago.edu>
2018-09-20 17:12:39 -0500

    return_index, match_and_set

--- diff.py
+++ diff.py
@@ -1,6 +1,8 @@
 from pdblib import parse_pdb_file, parse_pdb_line
 from .pdblist import PDBList
 from math import isclose
+from functools import partial
+from copy import deepcopy
 
 def equal(lhs, rhs):
     """
@@ -27,8 +29,14 @@ def entry_compare(lhs, rhs, cmp_keys = ["x", "y", "z", "element"]):
     """
     return not entry_match(lhs, rhs, cmp_keys)
 
+def entry_matcher(matching_keys):
+    return partial(entry_match, matching_keys=matching_keys)
+
+def entry_comparer(cmp_keys):
+    return partial(entry_compare, cmp_keys=cmp_keys)
+
 def diff_pdb(query, subject, match=entry_match, compare=entry_compare,
-             filter=None, exclude=None):
+             filter=None, exclude=None, return_index=False, return_pdb=False):
     """
     calculate the difference between pdb files
 
@@ -49,21 +57,30 @@ def diff_pdb(query, subject, match=entry_match, compare=entry_compare,
     # pdb1 < pdb2
     pdb1 = parse_pdb_file(query)
     pdb2 = parse_pdb_file(subject)
+    pdb1orig = deepcopy(pdb1)
+    pdb2orig = deepcopy(pdb2)
 
+    # if filter changes the order / number of pdb1, the returned index will
+    # be wrong
     if filter is not None:
         pdb1 = PDBList([filter(entry) for entry in pdb1])
 
     matchlist = {
             "query"   : PDBList(),
             "subject" : PDBList(),
+            "queryidx"   : [],
+            "subjectidx" : [],
                 }
     difflist = {
             "query"   : PDBList(),
             "subject" : PDBList(),
+            "queryidx"   : [],
+            "subjectidx" : [],
                }
     notfoundlist = PDBList()
+    notfoundindex = []
 
-    for entry in pdb1:
+    for idx1, entry in enumerate(pdb1):
         if exclude is not None and exclude(entry):
             continue
         found = False
@@ -71,13 +88,41 @@ def diff_pdb(query, subject, match=entry_match, compare=entry_compare,
             if match(entry, pdb2[idx2]):
                 matchlist["query"].append(entry)
                 matchlist["subject"].append(pdb2[idx2])
+                matchlist["queryidx"].append(idx1)
+                matchlist["subjectidx"].append(idx2)
                 if compare(entry, pdb2[idx2]):
                     difflist["query"].append(entry)
                     difflist["subject"].append(pdb2[idx2])
-                del pdb2[idx2]
+                    difflist["queryidx"].append(idx1)
+                    difflist["subjectidx"].append(idx2)
+                if not return_index:
+                    del pdb2[idx2]
                 found = True
                 break
         if not found:
             notfoundlist.append(entry)
+            if return_index:
+                notfoundindex.append(idx1)
 
-    return matchlist, difflist, notfoundlist
+    basic_return = (matchlist, difflist, notfoundlist)
+    pdb_return = (pdb1orig, pdb2orig) if return_pdb else ()
+    index_return = (notfoundindex,) if return_index else ()
+
+    return basic_return + pdb_return + index_return
+
+def match_and_set(iname, refname, changelist, change_func=None, **kwargs):
+    match, diff, unfound, pdbin, pdbref, nfoundidx = \
+        diff_pdb(iname, refname, return_index=True, return_pdb=True, **kwargs)
+
+    #print(diff["queryidx"], diff["subjectidx"])
+
+    # make sure pass by reference
+    assert issubclass(type(pdbin), list)
+
+    for idx, refpos in zip(diff["queryidx"], diff["subject"][changelist]):
+        for key, value in zip(changelist, refpos):
+            pdbin[idx][key] = value
+        if change_func is not None:
+            change_func(pdbin, idx)
+
+    return pdbin
--- pdblist.py
+++ pdblist.py
@@ -3,13 +3,22 @@ class PDBList(list):
     store as list of dict, but can function as a dict of list
     """
     def __getitem__(self, key):
-        #if type(key) is int:
-        try:
-            if abs(float(key) - int(key)) > 1e-6:
-                raise ValueError
-            return super().__getitem__(int(key))
-        except ValueError:
+        if type(key) is str:
+            #if key is str
             try:
                 return [x[key] for x in self]
             except:
                 return [[x[key] for x in y ] for y in self]
+        else:
+            try:
+                iter(key)
+            except TypeError:
+                #if key is not iterable or str
+                return super().__getitem__(key)
+        #if key is iterable but not str
+        return [[x[k] for k in key] for x in self]
+
+    def find_serial(self, serial):
+        for i, x in enumerate(self):
+            if x["serial"] == serial:
+                return i
